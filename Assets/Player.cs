65032b13f598f6532979f855ef33adbf91204c90
MAX-DTOP\Max <me@bla.com>
2018-09-06 04:30:24 -0400

    no message

---
+++ Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Color color;
+    public string name;
+    public float Score
+    {
+        get
+        {
+            return FindObjectsOfType<GameObject>().SelectMany(G=> G.GetComponents<IScore>()).Where(jscore=>jscore!=null).Select(jscore=>jscore.VictoryPoints).Sum();
+		}
+    }
+
+
+
+}
